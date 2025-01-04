3c181e463ef620b065cd5a57d9ef6f8e8b85cdb1
CarlosCorreia78 <cgi.carlos@gmail.com>
2025-01-04 06:08:01 +0000

    Add next-themes to apply dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import Header from "./components/Header";
+import ThemeCom from "./components/ThemeCom";
 
 
 
@@ -21,12 +23,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+        <ThemeProvider>   
+          <ThemeCom>
+           <Header />
+          {children}
+          </ThemeCom>   
+        </ThemeProvider>
       </body>
     </html>
   );
