47e5bfda5198a4b291ad1d63acde24c6a80670de
CarlosCorreia78 <cgi.carlos@gmail.com>
2025-01-15 23:31:57 +0000

    teste02

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "next-blog-bac5b.firebaseapp.com",
-  projectId: "next-blog-bac5b",
-  storageBucket: "next-blog-bac5b.firebasestorage.app",
-  messagingSenderId: "282459795765",
-  appId: "1:282459795765:web:9392a74d8014b012c2dc7c"
+  authDomain: "next-blog-3e72d.firebaseapp.com",
+  projectId: "next-blog-3e72d",
+  storageBucket: "next-blog-3e72d.firebasestorage.app",
+  messagingSenderId: "179445529086",
+  appId: "1:179445529086:web:238fd8886c6c15f23fe8af"
 };
 
 // Initialize Firebase
